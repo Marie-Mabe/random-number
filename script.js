c9e5ad48dadd6462b691b0fed57904e22da6f037
Mabé <topmabe@gmail.com>
2025-03-19 23:50:04 +0000

    update button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-console.log("Hi");
+console.log("script chargé");
 
 const randomNumberEl = document.querySelector("#random-number");
+console.log("Bouton trouvé ?", randomNumberEl);
 
-randomNumberEl.addEventListener("click", () => {    
-    document.querySelector("#result").textContent = Math.random();
+randomNumberEl.addEventListener("click", () => {
+    document.querySelector("#result").textContent = Math.floor(
+        getRandomNumber(1, 100)
+    );
 });
+
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
